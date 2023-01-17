b9ce3044af03807e07f768d4fc030f7a6c403814
sidbarret87 <sidbarret@mail.ru>
2023-01-17 18:53:06 +0300

    4 - Тестовый контроллер написан и маршрут настроен

--- samplesite/bboard/views.py
+++ samplesite/bboard/views.py
@@ -1,3 +1,6 @@
+from django.http import HttpResponse
 from django.shortcuts import render
 
+def index(request):
+    return HttpResponse("Здесь будет выведен список объявлений")
 # Create your views here.
