f169eee13377ae3cdfb3eb58297715dc5f9e6732
sidbarret87 <sidbarret@mail.ru>
2023-01-18 18:23:46 +0300

    6 - работа  моделями и orm

--- samplesite/bboard/views.py
+++ samplesite/bboard/views.py
@@ -1,6 +1,10 @@
 from django.http import HttpResponse
 from django.shortcuts import render
-
+from .models import Bb
 def index(request):
-    return HttpResponse("Здесь будет выведен список объявлений")
+    s='Список объявлений\r\n\r\n\r\n'
+    for bb in Bb.objects.order_by('-published'):
+        s+=bb.title + '\r\n' + bb.content + '\r\n\r\n'
+    return HttpResponse(s, content_type="text/plain; charset=utf-8")
+
 # Create your views here.
