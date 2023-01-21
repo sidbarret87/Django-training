b3388c001f376150cccd12a8ec8309a8372177a3
sidbarret87 <sidbarret@mail.ru>
2023-01-21 17:20:20 +0300

    7 - Создан шаблон и вебстраница на его основе

--- samplesite/bboard/views.py
+++ samplesite/bboard/views.py
@@ -1,10 +1,11 @@
 from django.http import HttpResponse
 from django.shortcuts import render
+from django.template import loader
 from .models import Bb
 def index(request):
-    s='Список объявлений\r\n\r\n\r\n'
-    for bb in Bb.objects.order_by('-published'):
-        s+=bb.title + '\r\n' + bb.content + '\r\n\r\n'
-    return HttpResponse(s, content_type="text/plain; charset=utf-8")
+    template = loader.get_template('bboard/index.html')
+    bbs = Bb.objects.order_by('-published')
+    context = {'bbs':bbs}
+    return render (request, 'bboard/index.html',{'bbs':bbs})
 
 # Create your views here.
