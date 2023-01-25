e1c4ac3d9c3b408bbcb9873f9919272991866dd9
sidbarret87 <sidbarret@mail.ru>
2023-01-25 11:44:19 +0300

    8 - Работа с админкой сайта Django завершена

--- samplesite/bboard/views.py
+++ samplesite/bboard/views.py
@@ -4,7 +4,7 @@ from django.template import loader
 from .models import Bb
 def index(request):
     template = loader.get_template('bboard/index.html')
-    bbs = Bb.objects.order_by('-published')
+    bbs = Bb.objects.all()
     context = {'bbs':bbs}
     return render (request, 'bboard/index.html',{'bbs':bbs})
 
