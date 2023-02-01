113ca9db5821053b3ece40ed120e7abed07ecb41
sidbarret87 <sidbarret@mail.ru>
2023-02-01 13:15:52 +0300

    9 - Работа со связями внешних ключей и обратным разрешением интернет адресов

--- samplesite/bboard/views.py
+++ samplesite/bboard/views.py
@@ -1,11 +1,17 @@
 from django.http import HttpResponse
 from django.shortcuts import render
 from django.template import loader
-from .models import Bb
+from .models import Bb, Rubric
 def index(request):
     template = loader.get_template('bboard/index.html')
     bbs = Bb.objects.all()
-    context = {'bbs':bbs}
-    return render (request, 'bboard/index.html',{'bbs':bbs})
-
+    rubrics= Rubric.objects.all()
+    context = {'bbs':bbs, 'rubrics':rubrics}
+    return render (request, 'bboard/index.html',context)
+def by_rubric(request, rubric_id):
+    bbs = Bb.objects.filter(rubric=rubric_id)
+    rubrics = Rubric.objects.all()
+    current_rubric=Rubric.objects.get(pk=rubric_id)
+    context = {'bbs':bbs,'rubrics':rubrics, 'current_rubric':current_rubric}
+    return render(request, 'bboard/by_rubric.html', context)
 # Create your views here.
